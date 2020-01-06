80b3848bb01fe32b6fabdb2856fc635f27daf79e
lujianmin <lujianmin6649@dingtalk.com>
2020-01-06 13:56:47 +0800

    初始化工程

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import HelloWorld from '@/components/HelloWorld';
 import Login from "@/components/Login";
-import BaseContent from "@/components/BaseContent";
+import BaseContent from "@/components/BaseConent";
 
 import ReaderMonitoring from "@/components/ReaderMonitoring/ReaderMonitoring";
 import ResultsQuery from "@/components/ResultsQuery/ResultsQuery";
